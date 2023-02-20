b56d9af362150dac8399c9f561f4556f0280270a
Janardhan Pulivarthi <j143@protonmail.com>
2023-02-21 01:17:15 +0530

    fix RuntimeError: There is no current event loop in thread 'Thread-6'.
    use asyncio

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 
 import threading
-
+import asyncio
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
@@ -24,20 +24,24 @@ db = SQLAlchemy(app)
 files = UploadSet('files', ALL)
 configure_uploads(app, files)
 
+
 class File(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
     path = db.Column(db.String(255))
     file_type = db.Column(db.String(50))
 
+
 class FileForm(FlaskForm):
     file = files
     file_type = StringField('File type', validators=[DataRequired()])
     submit = SubmitField('Upload')
 
+
 @app.route('/')
 def home():
     return render_template('home.html')
+
 
 # @app.route('/upload', methods=['POST'])
 # def upload_file():
@@ -84,22 +88,26 @@ def browse(files=None):
         files = File.query.all()
     return render_template('browse.html', files=files, form=form, file_type=file_type)
 
+
 @app.route('/browse/<int:file_id>')
 def view_file(file_id):
     # id = request.args.get('file_id')
     print(f"id={file_id}")
     file = File.query.get_or_404(file_id)
     if 1:
-    # if file.file_type == 'application/vnd.tcpdump.pcap':
+        # if file.file_type == 'application/vnd.tcpdump.pcap':
         # Extract packet details from pcap file
-        thread = threading.Thread(target=run_capture, args=(file.path))
+        thread = threading.Thread(target=run_capture, args=(file.path,))
         thread.start()
 
         return render_template('view_pcap.html', file=file)
     else:
         return render_template('view_file.html', file=file)
 
+
 def run_capture(file_path):
+    print("I am inside run_capture method")
+    asyncio.set_event_loop(asyncio.new_event_loop())
     capture = pyshark.FileCapture(file_path)
     packets = []
     for packet in capture:
@@ -115,7 +123,7 @@ def run_capture(file_path):
     file.packets = packets
     db.session.commit()
 
+
 if __name__ == '__main__':
     db.create_all()
     app.run(debug=True)
-
